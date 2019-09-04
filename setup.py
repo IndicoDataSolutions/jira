cd2daae7c35c3f44774be4033f6c22960455f311
Chris Lee <sihrc.c.lee@gmail.com>
2019-09-04 09:58:50 -0400

    FIX: upgrade requests

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author="Chris Lee",
     author_email="chris@indico.io",
     packages=["jira"],
-    install_requires=["requests==2.10.0", "click==7.0"],
+    install_requires=["requests==2.22.0", "click==7.0"],
     extras_require={"test": ["pytest"]},
     scripts=["bin/jira"],
 )
